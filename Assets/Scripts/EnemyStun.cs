0531f7a4ee3a85313291d258878d7816e3c7a09c
Pavel Perestoronin <eigenein@gmail.com>
2016-12-27 22:06:19 +0100

    Replace those ugly tabs with spaces

--- Assets/Scripts/EnemyStun.cs
+++ Assets/Scripts/EnemyStun.cs
@@ -2,15 +2,15 @@
 
 public class EnemyStun : MonoBehaviour {
 
-	// if Player hits the stun point of the enemy, then call Stunned on the enemy
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		if (other.gameObject.tag == "Player")
-		{
-			// tell the enemy to be stunned
-			this.GetComponentInParent<Enemy>().Stunned();
+    // if Player hits the stun point of the enemy, then call Stunned on the enemy
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // tell the enemy to be stunned
+            this.GetComponentInParent<Enemy>().Stunned();
 
-		    other.gameObject.GetComponent<CharacterController2D>().Bounce();
-		}
-	}
+            other.gameObject.GetComponent<CharacterController2D>().Bounce();
+        }
+    }
 }
